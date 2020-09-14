af8bdc45ec440020875a2479b99e63cb3f3d3147
Atilio Villalba <atiliovg94@gmail.com>
2020-09-14 13:24:57 -0400

    added conditionals

---
+++ 02 - Contionals.js
@@ -0,0 +1,27 @@
+//#region IF ELSE condition
+/*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*/
+//If else statements
+/*
+    if(condition)
+    else if(condition)
+    else
+*/
+/*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*/
+//Important IFs
+    var x;
+    if (x) console.log(x);
+/*
+    This checks for: null, undefined, NaN, empty string (""), 0, false
+*/
+//#endregion
+/*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*/
+//#region Boolean logic operators
+/*
+    AND     &&      All conditions must be met          true if ALL true
+    OR      ||      Only one condition must be met      true if ONE true
+    NOT     !       Returns the opposite                inverts true/false value
+*/
+//#endregion
+/*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*/
+
+
