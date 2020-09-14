ad0c97366f31765413e2eb6619ab780269c48651
Atilio Villalba <atiliovg94@gmail.com>
2020-09-14 10:07:36 -0400

    Initial commit

---
+++ 00 - Definitions.js
@@ -0,0 +1,11 @@
+/*
+JS us a lightweight cross-platform, object-oriented computed programing language.
+
+JS is one of the three core technologies of web development
+
+JS can be used for:
+    Client-side: in the browser.
+    Server-side: nodeJS.
+
+
+*/
