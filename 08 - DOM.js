0a299e068a871d52a342682045d25da8d1d8feec
Atilio Villalba <atiliovg94@gmail.com>
2020-09-19 18:19:21 -0400

    added enent listener

--- 08 - DOM.js
+++ 08 - DOM.js
@@ -5,12 +5,36 @@ Each element inside and html tag is a different DOM element that we can access w
 */
 //#endregion
 /*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*/
-//#region DOM Definition
+//#region querySelector
+//document.querySelector('')
 /*
+Lets us select elements of the DOM just like we on in CSS
+    # -> for id
+    . -> for class
 
 */
 //#endregion
 /*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*/
-
+//#region textContent
+//document.querySelector('').textContent = "Hello"
+/*
+Lets us set the text content of the selected element in plain text NO HTML, also interprets HTML as text.
+*/
+//#endregion
+/*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*/
+//#region innerHTML
+//document.querySelector('').innerHTML = "<em>Hello</em>" 
+/*
+Lets us set the text content of the selected element in HTML
+*/
+//#endregion
+/*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*/
+//#region EVENTS addEventListener()
+//document.querySelector('').addEventListener() = 
+/*
+Lets us detect when an event happened to the selected object (https://developer.mozilla.org/en-US/docs/Web/Events)
+*/
+//#endregion
+/*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*/
 
 
