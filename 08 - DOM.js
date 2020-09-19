e0098c3569191a449dc0760ce4c68771aac79988
Atilio Villalba <atiliovg94@gmail.com>
2020-09-19 17:27:46 -0400

    dom created

---
+++ 08 - DOM.js
@@ -0,0 +1,16 @@
+//#region DOM Definition
+/*
+DOM -> Document object
+Each element inside and html tag is a different DOM element that we can access with javascript
+*/
+//#endregion
+/*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*/
+//#region DOM Definition
+/*
+
+*/
+//#endregion
+/*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*/
+
+
+
