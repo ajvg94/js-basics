ab4f5ac19e8947cf44f1f917161fd97cc6c97a44
Atilio Villalba <atiliovg94@gmail.com>
2020-09-24 16:23:30 -0400

    added modern js

---
+++ 13 - Modern JS (npm, Babel, Webpack).js
@@ -0,0 +1,28 @@
+//#region NodeJs and npm
+/*
+NodeJS let's you execute javascript code outside of the browser environment
+npm is a bundle system that lets you create/download code packages and use them in your projects
+*/
+//#endregion
+/*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*/
+//#region Babel
+/*
+Let's you write code in ES6/ESNext and it does the traduction to ES5 for more browser compatibility
+*/
+//#endregion
+/*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*/
+//#region webpack
+/*
+Let's us bundle our modules for exporting
+*/
+//#endregion
+/*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*/
+//#region package.json
+/*
+In the package.json and in npm we have 2 types of packages libraries(dependencies) and devDependencies, you can differentiate them
+in the installation process in the following way
+
+npm install jquery --save (installs as dependency)
+npm install webpack --save-dev (installs as a devDependency)
+*/
+//#endregion
